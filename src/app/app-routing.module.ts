4996c11ead5e5bbd595b75789b9187ba958e9f35
rajnish42413 <rajnish42413@gmail.com>
2018-08-29 23:43:04 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { DetailsComponent } from './details/details.component';
+import { PostsComponent } from './posts/posts.component';
+import { CrudsqlComponent } from './crudsql/crudsql.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UsersComponent
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  },
+  {
+    path: 'crudsql',
+    component: CrudsqlComponent
+  },
+];;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
